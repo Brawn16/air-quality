9d2bce21cc5b94ee309586bad3c4a6a2a28946f6
Brawn16 <nickbrawn@hotmail.com>
2019-08-04 23:08:51 +0100

    catch error

--- src/App.js
+++ src/App.js
@@ -1,69 +1,79 @@
 import React, { Component } from "react";
 import { requestLocationData, getAllLocations } from "./api/api";
 import SearchTool from "./SearchTool";
+import Error from "./Error";
 import DisplayContainer from "./DisplayContainer";
 import "./App.css";
 
 class App extends Component {
-  state = {
-    locations: [],
-    selectedLocations: [],
-    selectedSuggestion: ""
-  };
+    state = {
+        locations: [],
+        selectedLocations: [],
+        selectedSuggestion: "",
+        hasError: false
+    };
 
-  selectSuggestion = selectedSuggestion => {
-    requestLocationData(selectedSuggestion)
-      .then(response => {
-        response.data.results.length &&
-          this.setState({
-            selectedLocations: [
-              ...this.state.selectedLocations,
-              response.data.results[0]
-            ]
-          });
-      });
-  };
+    selectSuggestion = selectedSuggestion => {
+        requestLocationData(selectedSuggestion)
+            .then(response => {
+                response.data.results.length &&
+                    this.setState({
+                        selectedLocations: [
+                            ...this.state.selectedLocations,
+                            response.data.results[0]
+                        ]
+                    });
+            })
+            .catch(error => {
+                // handle error
+                this.setState({
+                    hasError: true
+                });
+                console.error(error);
+            });
+    };
 
-  removeDisplayCard = (location)=>{
-    this.setState({
-      selectedLocations: this.state.selectedLocations.filter((res)=>{
-        return res.location !== location
-      })
-    })
-    
-  }
+    removeDisplayCard = location => {
+        this.setState({
+            selectedLocations: this.state.selectedLocations.filter(res => {
+                return res.location !== location;
+            })
+        });
+    };
 
-  componentDidMount() {
-   getAllLocations()
-      .then(response => {
-        // handle success
-        this.setState(
-          {
-            locations: response.data.results.map(item => item.location)
-          },
+    componentDidMount() {
+        getAllLocations()
+            .then(response => {
+                // handle success
+                this.setState({
+                    locations: response.data.results.map(item => item.location)
+                });
+            })
+            .catch(error => {
+                // handle error
+                console.log(error);
+            });
+    }
+    render() {
+        const { locations, selectedLocations } = this.state;
+
+        return (
+            <div className="App-container">
+                <h1>Compare your Air</h1>
+                <p>compare the air quality between cities in the UK.</p>
+                <p>Select cities to compare using the search tool below. </p>
+                <SearchTool
+                    options={locations}
+                    selectSuggestion={this.selectSuggestion}
+                />
+                {this.state.hasError && <Error />}
+                <DisplayContainer
+                    results={selectedLocations}
+                    removeDisplayCard={this.removeDisplayCard}
+                />
+            </div>
         );
-      })
-      .catch(error => {
-        // handle error
-        console.log(error);
-      });
-  }
-  render() {
-    const { locations, selectedLocations } = this.state;
-    
-    return (
-      <div className="App-container">
-        <h1>Compare your Air</h1>
-        <p>compare the air quality between cities in the UK.</p>
-        <p>Select cities to compare using the search tool below. </p>
-        <SearchTool
-          options={locations}
-          selectSuggestion={this.selectSuggestion}
-        />
-        <DisplayContainer results={selectedLocations} removeDisplayCard={this.removeDisplayCard} />
-      </div>
-    );
-  }
+    }
 }
 
 export default App;
---
+++ src/Error.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = () => {
+    return (
+        <div>
+            <p>There is an Error fetching your request</p>
+        </div>
+    );
+};
+
+export default Error;
